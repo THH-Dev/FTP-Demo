4e8088ef34bb6787a1176ff379b72ff67861d478
John Nguyen <tuanken92@gmail.com>
2022-08-24 17:52:36 +0700

    Add project files.

---
+++ MainForm.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace FTP_Demo
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnLoad = new System.Windows.Forms.Button();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.BtnUpload = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // BtnLoad
+            // 
+            this.BtnLoad.Location = new System.Drawing.Point(622, 13);
+            this.BtnLoad.Name = "BtnLoad";
+            this.BtnLoad.Size = new System.Drawing.Size(75, 23);
+            this.BtnLoad.TabIndex = 0;
+            this.BtnLoad.Text = "Load";
+            this.BtnLoad.UseVisualStyleBackColor = true;
+            this.BtnLoad.Click += new System.EventHandler(this.BtnLoad_Click);
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(12, 15);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(579, 20);
+            this.txtPath.TabIndex = 1;
+            // 
+            // BtnUpload
+            // 
+            this.BtnUpload.Location = new System.Drawing.Point(713, 13);
+            this.BtnUpload.Name = "BtnUpload";
+            this.BtnUpload.Size = new System.Drawing.Size(75, 23);
+            this.BtnUpload.TabIndex = 0;
+            this.BtnUpload.Text = "Upload";
+            this.BtnUpload.UseVisualStyleBackColor = true;
+            this.BtnUpload.Click += new System.EventHandler(this.BtnUpload_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.BtnUpload);
+            this.Controls.Add(this.BtnLoad);
+            this.Name = "MainForm";
+            this.Text = "Main";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnLoad;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Button BtnUpload;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,38 @@
+﻿using JohnNguyen.Lib;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FTP_Demo
+{
+    public partial class MainForm : Form
+    {
+        string file_name;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnLoad_Click(object sender, EventArgs e)
+        {
+            file_name = MyLib.OpenFileDialog(eTypeFile.File_jpg);
+            if (file_name == null)
+                return;
+
+            txtPath.Text = file_name;
+
+        }
+
+        private void BtnUpload_Click(object sender, EventArgs e)
+        {
+            MyLib.UploadImage(file_name);
+        }
+    }
+}
